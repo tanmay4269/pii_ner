5b777cf36d6640ee68eb89aac13c153645137d88
Tanmay -- biplab48gb <tanmay4269@gmail.com>
2025-11-25 17:55:13 +0530

    fixed augmentation bug

--- 04-augment_data.py
+++ 04-augment_data.py
@@ -32,27 +32,50 @@ def digits_to_spoken(text: str) -> str:
 
 def augment_utterance(obj: dict) -> dict:
     text = obj['text']
-    entities = sorted(obj.get('entities', []), key=lambda x: x['start'], reverse=True)
+    entities = obj.get('entities', [])
+    
+    # Sort by start position (ascending) to process left-to-right
+    sorted_entities = sorted(enumerate(entities), key=lambda x: x[1]['start'])
     
     new_text = text
-    new_entities = []
+    new_entities = [ent.copy() for ent in entities]
+    cumulative_offset = 0
     
-    for ent in entities:
+    for orig_idx, ent in sorted_entities:
         label = ent['label']
-        start, end = ent['start'], ent['end']
-        span_text = text[start:end]
+        orig_start, orig_end = ent['start'], ent['end']
+        
+        # Adjust for previous augmentations
+        adj_start = orig_start + cumulative_offset
+        adj_end = orig_end + cumulative_offset
+        span_text = new_text[adj_start:adj_end]
         
         if label in ('CREDIT_CARD', 'PHONE') and re.search(r'\d', span_text):
             spoken = digits_to_spoken(span_text)
-            new_text = new_text[:start] + spoken + new_text[end:]
-            new_entities.append({'start': start, 'end': start + len(spoken), 'label': label})
+            new_text = new_text[:adj_start] + spoken + new_text[adj_end:]
+            
+            length_diff = len(spoken) - (orig_end - orig_start)
+            
+            # Update this entity's positions
+            new_entities[orig_idx]['start'] = adj_start
+            new_entities[orig_idx]['end'] = adj_start + len(spoken)
+            
+            # Update all entities that come after this one
+            for other_idx, other_ent in enumerate(entities):
+                if other_ent['start'] > orig_start:
+                    new_entities[other_idx]['start'] += length_diff
+                    new_entities[other_idx]['end'] += length_diff
+            
+            cumulative_offset += length_diff
         else:
-            new_entities.append(ent.copy())
+            # Still need to apply cumulative offset
+            new_entities[orig_idx]['start'] = adj_start
+            new_entities[orig_idx]['end'] = adj_end
     
     return {
         'id': obj['id'] + '_aug',
         'text': new_text,
-        'entities': list(reversed(new_entities))
+        'entities': new_entities
     }
 
 def main():
